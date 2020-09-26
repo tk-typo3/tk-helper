68e1391f5e93fbbf7bc13dd055a6f066577dddc6
Timon Kreis <mail@timonkreis.de>
2020-09-26 14:59:15 +0200

    .editorconfig updated

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,21 +7,21 @@
  * @var string $_EXTKEY
  */
 $EM_CONF[$_EXTKEY] = [
-	'title' => 'TK Helper',
-	'description' => '',
-	'category' => 'misc',
-	'author' => 'Timon Kreis',
-	'author_email' => 'mail@timonkreis.de',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '1.0.0',
-	'constraints' => [
-		'depends' => [
-			'typo3' => '9.5.0-9.5.99'
-		],
-		'conflicts' => [],
-		'suggests' => []
-	]
+    'title' => 'TK Helper',
+    'description' => '',
+    'category' => 'misc',
+    'author' => 'Timon Kreis',
+    'author_email' => 'mail@timonkreis.de',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '9.5.0-9.5.99'
+        ],
+        'conflicts' => [],
+        'suggests' => []
+    ]
 ];
--- ext_localconf.php
+++ ext_localconf.php
@@ -7,34 +7,34 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 if (!function_exists('tk_group_fields')) {
-	function tk_group_fields(array &$tableTCA, array $fields, string $paletteName = null, string $label = null) : void {
-		if ($paletteName === null) {
-			$paletteName = uniqid();
-		}
+    function tk_group_fields(array &$tableTCA, array $fields, string $paletteName = null, string $label = null) : void {
+        if ($paletteName === null) {
+            $paletteName = uniqid();
+        }
 
-		// Create palette
-		$tableTCA['palettes'][$paletteName]['showitem'] = implode(', ', $fields);
+        // Create palette
+        $tableTCA['palettes'][$paletteName]['showitem'] = implode(', ', $fields);
 
-		if ($label) {
-			$tableTCA['palettes'][$paletteName]['label'] = $label;
-		}
+        if ($label) {
+            $tableTCA['palettes'][$paletteName]['label'] = $label;
+        }
 
-		$paletteInserted = false;
-		$availableFields = array_map('trim', explode(',', $tableTCA['types']['1']['showitem']));
-		$filteredFields = [];
+        $paletteInserted = false;
+        $availableFields = array_map('trim', explode(',', $tableTCA['types']['1']['showitem']));
+        $filteredFields = [];
 
-		foreach ($availableFields as $field) {
-			if (in_array($field, $fields)) {
-				if (!$paletteInserted) {
-					$filteredFields[] = '--palette--;;' . $paletteName;
-					$paletteInserted = true;
-				}
-			}
-			else {
-				$filteredFields[] = $field;
-			}
-		}
+        foreach ($availableFields as $field) {
+            if (in_array($field, $fields)) {
+                if (!$paletteInserted) {
+                    $filteredFields[] = '--palette--;;' . $paletteName;
+                    $paletteInserted = true;
+                }
+            }
+            else {
+                $filteredFields[] = $field;
+            }
+        }
 
-		$tableTCA['types']['1']['showitem'] = implode(', ', $filteredFields);
-	}
+        $tableTCA['types']['1']['showitem'] = implode(', ', $filteredFields);
+    }
 }
