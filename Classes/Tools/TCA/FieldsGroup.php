18d4dde18e6fb2a49267b381e2d31d1e8ae1e835
Timon Kreis <mail@timonkreis.de>
2020-09-26 17:43:00 +0200

    License information added

--- Classes/Tools/TCA/FieldsGroup.php
+++ Classes/Tools/TCA/FieldsGroup.php
@@ -2,6 +2,7 @@
 /**
  * @author Timon Kreis <mail@timonkreis.de>
  * @copyright by Timon Kreis - All rights reserved
+ * @license http://www.opensource.org/licenses/mit-license.html
  */
 declare(strict_types = 1);
 
